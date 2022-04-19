194e1e700196f4783a513d2fccb25b71a1a027f7
alexei <alexei.saron@gmail.com>
2022-04-19 15:51:08 +0200

    start page data

--- public/js/miFiles/footerInfo.js
+++ public/js/miFiles/footerInfo.js
@@ -3,7 +3,6 @@ let footerListLinks = ''
 for (const [key, ArrValue] of Object.entries(rep)) { //  target="_blank"
     let image = `<a href="/`+ArrValue[2]+`" target="_blank"><img alt="`+ArrValue[0]+`" src="/img/`+ArrValue[1]+`" width="33px" /></a>`
     footerListLinks += image
-
 }
 
 let footerInfo = footerListLinks + `
--- public/js/miFiles/rep.js
+++ public/js/miFiles/rep.js
@@ -8,16 +8,25 @@ var rep = {
     ],
     'pervyj-kanal': [
         'Первый смотреть онлайн', 'pervyj.png', '#pervyj-kanal',
-        ''
+        'Request URL: https://s103.ontvtime.ru/stream/FFA0945BCCDF67161820222426283044/f48c3aca0a9a866336bd093a196870bfdsMseFrag9817389?buf=1'
     ],
     'ntv-tv-online': [
         'НТВ онлайн', 'ntv.png', '#ntv-tv-online',
         'https://a787201477-s91412.cdn.ngenix.net/mdrm/CH_NTV/bw2000000/playlist.m3u8?utcstart=1649966400&version=2'
     ],
+    'tnt-online': [
+        'Телеканал ТНТ смотреть онлайн','tnt.png','#tnt-online',
+        'https://hzv4.sysnet.top/video/lniMJx7dSWu-wRVg_3HcFw/11/playlist.m3u8'
+    ],
     'tv-rossia-24-online': [
         'Россия 24 онлайн','rossija_24.png','#tv-rossia-24-online',
         'https://a787201472-s91412.cdn.ngenix.net/mdrm/CH_RUSSIA24/bw2000000/playlist.m3u8?utcstart=1650222000&version=2'
-    ]
+    ],
+    'sts-channel':[
+        'Телеканал СТС онлайн','sts.png','#sts-channel',
+        'https://hzv6.sysnet.top/video/lniMJx7dSWu-wRVg_3HcFw/9/playlist.m3u8'
+    ],
+    
 
 }
 
